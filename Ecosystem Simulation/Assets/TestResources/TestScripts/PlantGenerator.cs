57c7a1af370d08ed2ae28aece937a2c44f8b7936
R4falek <rafal888@onet.pl>
2022-12-12 14:42:49 +0100

    update

--- Ecosystem Simulation/Assets/TestResources/TestScripts/PlantGenerator.cs
+++ Ecosystem Simulation/Assets/TestResources/TestScripts/PlantGenerator.cs
@@ -4,39 +4,41 @@ using UnityEngine;
 
 public class PlantGenerator : MonoBehaviour
 {
-    public GameObject plantToPlace;
-    public GameObject terrain;
-    public Vector3 breaksSizeX = new Vector3(0, 0, 5);
-    public Vector3 breaksSizeY = new Vector3(5, 0, 0);
-    public int x = 1;
-    public GameObject plantTest;
+    public List<GameObject> plantList;
+    public float chunkSize = 10;
+    private int x = 1;
+    private GameObject plantTest;
 
     void Start()
     {   
-        transform.position = new Vector3(1, 320, 1);
-        plantTest = Instantiate(plantToPlace);
-        for(int i = 0; i < 4000; i++){
-            Ray ray = new Ray (transform.position, -transform.up);
-            RaycastHit hitInfo;
+        // plantTest = Instantiate(plantToPlace);
+        for(int j = 0 ; j < plantList.Count; j++){
+            transform.position = new Vector3(1, 270, 1);
+            GameObject plantToPlace = plantList[j];
+            for(int i = 0; i < (40000 * 5) / chunkSize; i++){
+                Vector3 rayPosition = transform.position + new Vector3(NextFloat(0, chunkSize/2), 0, NextFloat(0, chunkSize/2));
+                Ray ray = new Ray (rayPosition, -transform.up);
+                RaycastHit hitInfo;
 
-            if(Physics.Raycast (ray, out hitInfo, 1000)){
-                // Debug.DrawLine (ray.origin, hitInfo.point, Color.red);
-                GameObject plant = Instantiate(plantToPlace);
-                // plant.transform.parent = plantToPlace.transform;
-                if(plant.GetComponent<Plant>().isSpawned(hitInfo.distance)){
-                    plant.transform.position = hitInfo.point;
-                    plant.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+                if(Physics.Raycast (ray, out hitInfo, 1000)){
+                    // Debug.DrawLine (ray.origin, hitInfo.point, Color.red);
+                    GameObject plant = Instantiate(plantToPlace);
+                    // plant.transform.parent = plantToPlace.transform;
+                    if(plant.GetComponent<Plant>().isSpawned(hitInfo.distance)){
+                        plant.transform.position = hitInfo.point;
+                        plant.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+                    }
+                    else{
+                        Destroy(plant);
+                    }
+                    
+                    transform.position = transform.position + x * new Vector3(chunkSize, 0, 0);     
                 }
                 else{
-                    Destroy(plant);
+                    x *= -1;
+                    transform.position = transform.position + new Vector3(0, 0, chunkSize);
+                    transform.position = transform.position + x * new Vector3(chunkSize, 0, 0);
                 }
-                
-                transform.position = transform.position + x * breaksSizeX;     
-            }
-            else{
-                x *= -1;
-                transform.position = transform.position + breaksSizeY;
-                transform.position = transform.position + x * breaksSizeX;
             }
         }
     }
@@ -51,5 +53,11 @@ public class PlantGenerator : MonoBehaviour
         //         Debug.DrawLine (ray.origin, hitInfo.point, Color.red);
         //         // Debug.Log(hitInfo.distance);
         //     }
+    }
+
+    static float NextFloat(float min, float max){
+        System.Random random = new System.Random();
+        double val = (random.NextDouble() * (max - min) + min);
+        return (float)val;
     }
 }
